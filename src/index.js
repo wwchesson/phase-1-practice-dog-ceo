a1bf762a93a3a0ddbd10682f1e55cd8afda02a95
Will <willchesson@gmail.com>
2022-02-16 14:38:07 -0500

    commit tree 7344b538d11ab0bdc2a558ce8f3f30193f31fdf5

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-console.log('%c HI', 'color: firebrick')
+let breeds = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    getDogs();
+    getBreeds();
+    getLetter();
+});
+
+function getDogs () {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then (dogData => {
+        dogData.message.forEach(image => renderDogImage(image))
+    });
+}
+
+function renderDogImage (dogPic) {
+    let dogContainer = document.querySelector("#dog-image-container");
+    let newImage = document.createElement('img');
+    newImage.src = dogPic;
+    dogContainer.appendChild(newImage);
+
+}
+
+function getBreeds() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(res => res.json())
+    .then(breedData => {
+        breeds = Object.keys(breedData.message)
+        breeds.forEach(breed => loadBreedData(breed))
+    });
+}
+
+
+function loadBreedData (breed) {
+     let breedId = document.querySelector("#dog-breeds")
+     let newBreed = document.createElement('li')
+     newBreed.className = "dogName"
+     newBreed.innerText = breed;
+     newBreed.style.cursor = 'pointer';
+     breedId.appendChild(newBreed)
+     newBreed.addEventListener('click', newColor)
+}
+
+function newColor(event) {
+    event.target.style.color = 'red';
+} 
+
+function getLetter () {
+    const dropDown = document.querySelector("#breed-dropdown")
+    let newBreed = document.createElement('li')
+    dropDown.addEventListener("change", (event) => {
+        let dropLetter = event.target.value
+        let getLi = document.querySelectorAll("li.dogName")
+    
+        let breedFilter = breeds.filter(breed => breed.startsWith(dropLetter))
+        getLi.forEach(breed => breed.remove())
+        breedFilter.forEach(breed => loadBreedData(breed))
+    })
+}
